c35e785edb75bf056d6c45de76ca277fa654c97b
Abhinav Sharma <abhinavsharma7686@gmail.com>
2026-07-10 13:35:04 +0530

    Refactor timestamp functions for better usability
    added the get_current_utc_timestamp fucntion and also improved the exception handling
    
    Signed-off-by: Abhinav Sharma <abhinavsharma7686@gmail.com>

--- collector/utils/timestamp.py
+++ collector/utils/timestamp.py
@@ -1,6 +1,9 @@
 import random
 from datetime import datetime, timezone, timedelta
 
+def get_current_utc_timestamps():
+    current_time = datetime.now(timezone.utc)
+    return iso8601_formatting(current_time)
 
 def iso8601_formatting(timestamp):
     iso_string = timestamp.isoformat(timespec='seconds').replace('+00:00', 'Z')
@@ -9,6 +12,8 @@ def iso8601_formatting(timestamp):
 
 # This function generates a single timestamp
 def generate_random_timestamp(start_time, end_time):
+    if start_time >= end_time:
+        raise ValueError("start_time must be before end_time")
     time_difference = end_time - start_time
     total_seconds = int(time_difference.total_seconds())
     random_seconds = random.randint(0, total_seconds)
@@ -17,13 +22,15 @@ def generate_random_timestamp(start_time, end_time):
 
 
 # This function will create a loop and make a list of desired number of timestamps
-def random_timestamps(num_of_timestamps):
-    start = datetime(2026, 7, 1, 10, 0, 0, tzinfo=timezone.utc)
-    end = datetime(2026, 7, 1, 12, 0, 0, tzinfo=timezone.utc)
-
-    for timestamp in range(0, num_of_timestamps):
-        print(iso8601_formatting(generate_random_timestamp(start, end)))
+def random_timestamps(start_time, end_time, num_of_timestamps):
+    timestamps = []
+    for timestamp in range(num_of_timestamps):
+        timestamps.append(iso8601_formatting(generate_random_timestamp(start_time, end_time)))
+    return timestamps
 
 
 if __name__ == "__main__":
-    print(random_timestamps(10))
+    start = datetime(2026, 7, 1, 10, 0, 0, tzinfo=timezone.utc)
+    end = datetime(2026, 7, 1, 12, 0, 0, tzinfo=timezone.utc)
+
+    print(random_timestamps(start, end, 10))
